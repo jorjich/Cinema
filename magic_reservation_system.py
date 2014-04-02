50d7e4c44c3e557c8cc48c4c953b063a9bcad770
Cvetan <crusenov@gmail.com>
2014-04-02 14:14:53 +0300

    Initial commit of cinema class

---
+++ magic_reservation_system.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+
+class Cinema():
+    """docstring for Cinema"""
+
+
+    def show_movies(self):
+
+
+    def show_movies_projections(self, movie_id, date = 0):
+
+
+    def make_reservaton(self):
