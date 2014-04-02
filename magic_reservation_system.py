8573adab12874568274f53790e2d9f513932094a
Cvetan <crusenov@gmail.com>
2014-04-02 15:16:28 +0300

    add smth

--- magic_reservation_system.py
+++ magic_reservation_system.py
@@ -7,8 +7,38 @@ class Cinema():
 
     def show_movies(self):
 
+        conn = sqlite3.connect("cinema_database.db")
+        cursor = conn.cursor()
 
-    def show_movies_projections(self, movie_id, date = 0):
+        result = cursor.execute('''SELECT name
+                                   FROM movies
+                                   ORDER BY rating DESC''')
+
+        for i, row in enumerate(result):
+            print("{" + str(i+1) + "} -", row[0])
+
+        conn.commit()
+        conn.close()
 
 
-    def make_reservaton(self):
+    # def show_movies_projections(self, movie_id, date = 0):
+
+
+    # def make_reservaton(self):
+
+    def loop(self):
+        c = Cinema()
+        command = input(">")
+        cmd = command
+
+        while True:
+            if cmd == 'sh':
+                c.show_movies()
+                break
+
+def main():
+    new = Cinema()
+    new.loop()
+
+if __name__ == '__main__':
+    main()
