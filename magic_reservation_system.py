1e128c96c140e586b708f05646271002bb2e6ba1
Cvetan <crusenov@gmail.com>
2014-04-03 14:26:19 +0300

    Add new features!

--- magic_reservation_system.py
+++ magic_reservation_system.py
@@ -69,7 +69,9 @@ class Cinema():
                                        FROM movies
                                        WHERE movies_id = ?''', (movie_id))
         for row in movie_name:
-            print("Projection for movie \'" + row[0] + "\':")
+            name_of_the_movie = row[0]
+        print("Projection for movie \'" + name_of_the_movie + "\':")
+
 
         m.show_movies_projections(movie_id)
         print()
@@ -98,7 +100,6 @@ class Cinema():
         for i,row in enumerate(hall):
             print(''.join(row))
 
-        numbers_like_strings = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
         counter = 0
         while int(number_of_tickets)> counter:
             choose_seat = (input("Step 4 (Seats): Choose seat %d>"%(counter+1)))
@@ -107,14 +108,29 @@ class Cinema():
             col_choice = int(users_choice[1])
             if(row_choice<11 and row_choice>0 and col_choice<11 and col_choice>0):
                 if (hall[row_choice][col_choice] == ".  "):
-                    print("pishem v bazata")
-                    new_seat = cursor.execute('''INSERT INTO reservations(username, projection_id, row, col) VALUES(?, ?, ?, ?) ''', (name, projection_id, seat_row, seat_col))
+                    new_seat = cursor.execute('''INSERT INTO reservations(username, projection_id, row, col) VALUES(?, ?, ?, ?) ''', (name, projection_id, row_choice, col_choice))
 
                     counter += 1
                 else:
                     print("The plase is taken. Try again!")
             else:
                 print("The place is incorrect! Try again!")
+        print("This is your reservation:")
+        select_movie_name = cursor.execute('''SELECT name, rating
+                                              FROM movies
+                                              WHERE movies_id = ?''', (movie_id))
+        for row in select_movie_name:
+            select_name = row[0]
+            select_rating = str(row[1])
+        print("Movie:\"" + select_name + "\" (" + select_rating + ")")
+        # select_date_time = cursor.execute('''SELECT date, time, type
+        #                                      FROM projections
+        #                                      WHERE proj_id = ? ''', (projection_id))
+        # for row in select_date_time:
+
+        # print("Date and Time: " 2014-04-02 19:30 (2D))
+
+
 
         conn.commit()
         conn.close()
